f3e62798d88e647b5d86032245c9d4baa00532c9
Vincent Privat <vincent.privat@gmail.com>
2017-11-28 19:54:02 +0100

    update to JOSM 12643

--- src/andrei/josm/plugin/color/ColorPlugin.java
+++ src/andrei/josm/plugin/color/ColorPlugin.java
@@ -1,6 +1,6 @@
 package andrei.josm.plugin.color;
 
-import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
@@ -9,8 +9,6 @@ public class ColorPlugin extends Plugin {
 
     public ColorPlugin(final PluginInformation info) {
         super(info);
-        MainMenu.add(Main.main.menu.moreToolsMenu, new ColorAction());
+        MainMenu.add(MainApplication.getMenu().moreToolsMenu, new ColorAction());
     }
-
-
 }
